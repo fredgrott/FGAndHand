d2fe90dbf6840a181c026b8d44b2939cdc0bf5c7
Fred.Grott <fred.grott@gmail.com>
2011-06-04 08:58:21 -0500

    first commit

---
+++ src/com/fredgrott/fgandhand/MainAcitivity.java
@@ -0,0 +1,15 @@
+package com.fredgrott.fgandhand;
+
+import com.fredgrott.andhand.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainAcitivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
